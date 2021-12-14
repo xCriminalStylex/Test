018cfa6631f1038717db4d6d4c59713382bbd1fa
xCriminalStylex <oleksandrzikran@gmail.com>
2021-12-14 06:39:37 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practic
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		class MyClass
+		{
+			public bool IsLuckyTicket(string ticket)
+			{
+				int a;
+				a = 5;
+			}
+		}
+	}
+}
