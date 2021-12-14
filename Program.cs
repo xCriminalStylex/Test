4889abe5e56ce1038327ea5ee5c61d818b2903cb
xCriminalStylex <oleksandrzikran@gmail.com>
2021-12-14 10:08:16 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -6,14 +6,41 @@ namespace Practic
 	{
 		static void Main(string[] args)
 		{
-			
+			MyClass ticket = new MyClass();
+			ticket.IsLuckyTicket(Console.ReadLine());
 		}
 
 		class MyClass
 		{
+			public MyClass()
+            {
+				
+            }
+
 			public bool IsLuckyTicket(string ticket)
 			{
-				return false;
+				int Ticket = Convert.ToInt32(ticket);
+				int firstThree = Ticket / 1000;
+				int secondTree = Ticket % 1000;
+				int result1 = 0;
+				int result2 = 0;
+				for (int i = 0; i < 3; i++)
+                {
+				    result1 += firstThree % 10;
+					firstThree /= 10;
+					result2 += secondTree % 10;
+					secondTree /= 10;
+				}
+				if (result1 == result2)
+				{
+					Console.WriteLine("Lucky");
+					return true;
+				}
+				else
+				{
+					Console.WriteLine("Not lucky");
+					return false;
+				}
 			}
 		}
 	}
